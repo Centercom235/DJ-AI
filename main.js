ebe29fc7e9d4bf776b9dc81064d2ad6fbd257f6c
Centercom235 <79948838+Centercom235@users.noreply.github.com>
2022-05-11 20:53:45 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-song="";
+song1="";
+song2="";
 leftwristX=0;
 leftwristY=0;
 rightwristX=0;
 rightwristY=0;
 
 function preload(){
-song=loadSound("poetry One.mp3");
+song1=loadSound("music1.mp3");
+song2=loadSound("music2.mp3");
 }
 
 function setup(){
-canvas=createCanvas(600, 450);
+canvas=createCanvas(550, 400);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
@@ -18,7 +20,7 @@ poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-image(video, 0, 0, 600, 450);
+image(video, 0, 0, 550, 400);
 }
 
 function play(){
